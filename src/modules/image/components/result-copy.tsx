0cbc44dec90d255394dd6bf490ff43ee22a05433
Faustino Zanetto <faustizanetto@gmail.com>
2022-07-31 18:52:50 -0300

    Fixed missing prop.

--- src/modules/image/components/result-copy.tsx
+++ src/modules/image/components/result-copy.tsx
@@ -1,10 +1,8 @@
 import { trackEvent } from '@lib/google';
-import {
-  copyImageToClipboard,
-  handleImageGeneration,
-  saveImageToFile,
-} from '@lib/image-generation';
+import { copyImageToClipboard, handleImageGeneration, saveImageToFile } from '@lib/image-generation';
+import { selectBackgroundColor } from '@state/slices/toply.slice';
 import React, { useCallback } from 'react';
+import { useSelector } from 'react-redux';
 
 interface ResultCopyProps {
   resultRef: React.RefObject<HTMLDivElement>;
@@ -12,45 +10,44 @@ interface ResultCopyProps {
 
 const ResultCopy: React.FC<ResultCopyProps> = (props) => {
   const { resultRef } = props;
+  const backgroundColor = useSelector(selectBackgroundColor);
 
   /**
    * Handle the export of the image
    */
-  const handleCopy = useCallback(() => {
+  const handleCopy = useCallback(async () => {
     if (resultRef && resultRef.current) {
-      return handleImageGeneration(resultRef.current, true).then(
-        async (dataUrl) => {
-          try {
-            if (dataUrl) {
-              copyImageToClipboard(dataUrl);
-              trackEvent('Home', 'Save Photo');
-            }
-          } catch (error) {
-            console.error(error);
+      return await handleImageGeneration(resultRef.current, backgroundColor, true).then(async (dataUrl) => {
+        try {
+          if (dataUrl) {
+            await copyImageToClipboard(dataUrl);
+            trackEvent('Home', 'Save Photo');
           }
+        } catch (error) {
+          console.error(error);
         }
-      );
+      });
     }
-  }, [resultRef]);
+  }, [backgroundColor, resultRef]);
 
   return (
     <button
-      className='transition-colors inline-flex items-center justify-center p-2 overflow-hidden text-lg font-semibold text-white rounded-lg bg-red-700 hover:bg-red-600 '
-      aria-label='Copy Image'
+      className="transition-colors inline-flex items-center justify-center p-2 overflow-hidden text-lg font-semibold text-white rounded-lg bg-red-700 hover:bg-red-600 "
+      aria-label="Copy Image"
       onClick={handleCopy}
     >
       <svg
-        xmlns='http://www.w3.org/2000/svg'
-        className='h-8 w-8'
-        fill='none'
-        viewBox='0 0 24 24'
-        stroke='currentColor'
-        strokeWidth='2'
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-8 w-8"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="2"
       >
         <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          d='M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2'
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
         />
       </svg>
     </button>
