130eb403e781fcc987c24dc99fe6f261de6b5d0a
Faustino Zanetto <faustizanetto@gmail.com>
2022-09-05 21:53:29 -0300

    Fixed padding

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className="scroll-smooth p-0">
+      <Html lang="en" className="scroll-smooth" style={{ padding: '0 !important' }}>
         <Head />
         <body>
           <Main />
