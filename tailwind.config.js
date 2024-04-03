8602c6a95a1ad2b8ce126e4955f399bf501b05b1
Faustino Zanetto <zanettofaustino@gmail.com>
2024-04-03 14:47:20 -0300

    feat(web): added css vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,47 @@ module.exports = {
   content: ['./src/modules/**/*.{js,jsx,ts,tsx}', './src/pages/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      container: {
+        center: true,
+        screens: {
+          '2xl': '1400px',
+        },
+      },
       colors: {
-        primary: colors.indigo,
-        secondary: colors.purple,
-        danger: colors.red,
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        'background-alternate': 'hsl(var(--background-alternate))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
+          foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
       },
       fontFamily: {
         sans: ['var(--font-sans)'],
